4a531728d07318df10012e874057943da659fa83
Eric Tsai <chunchieh@gmail.com>
2020-03-28 22:14:12 +0800

    support trade partner pagination. add Link header

--- apps/commodities/views.py
+++ apps/commodities/views.py
@@ -9,16 +9,21 @@ from rest_framework import viewsets
 from rest_framework import status
 from rest_framework import pagination
 from rest_framework.response import Response
+from rest_framework.settings import api_settings
+from rest_framework.utils.urls import replace_query_param
 
 from apps.commodities import serializers
 from apps.commodities.models import TradePartner, Commodity, Inventory
 
 # Pagination
 class LinkHeaderPagination(pagination.BasePagination):
+    page_size = api_settings.PAGE_SIZE
+    page_query_param = 'page'
+    page_size_query_param = 'page_size'
 
     def paginate_queryset(self, queryset, request, view=None):
-        page_num = request.query_params.get('page', 1)
-        page_size = request.query_params.get('page_size', 20)
+        page_num = request.query_params.get(self.page_query_param, 1)
+        page_size = request.query_params.get(self.page_size_query_param, self.page_size) or 30
 
         try:
             self.page = Paginator(queryset, page_size).page(page_num)
@@ -29,7 +34,57 @@ class LinkHeaderPagination(pagination.BasePagination):
         return self.page.object_list
 
     def get_paginated_response(self, data):
-        return Response(data)
+        headers = {
+            'Link': self._get_link_header()
+        }
+        return Response(data, headers=headers)
+
+    def _get_link_header(self):
+        links = []
+        links.append(self.get_next_link())
+        links.append(self.get_last_link())
+        links.append(self.get_first_link())
+        links.append(self.get_prev_link())
+
+        return ', '.join(filter(lambda x: bool(x), links))
+
+    def get_next_link(self):
+        if not self.page.has_next():
+            return None
+
+        page_num = self.page.next_page_number()
+        page_size = self.page.paginator.per_page
+        return self.get_link(page_num, page_size, 'next')
+
+    def get_prev_link(self):
+        if not self.page.has_previous():
+            return None
+
+        page_num = self.page.previous_page_number()
+        page_size = self.page.paginator.per_page
+        return self.get_link(page_num, page_size, 'prev')
+
+    def get_first_link(self):
+        first_page_num = 1
+        if self.page.number <= first_page_num: # is_first_page
+            return None
+
+        page_size = self.page.paginator.per_page
+        return self.get_link(first_page_num, page_size, 'first')
+
+    def get_last_link(self):
+        last_page_num = self.page.paginator.num_pages
+        if self.page.number >= last_page_num: # is_last_page
+            return None
+
+        page_size = self.page.paginator.per_page
+        return self.get_link(last_page_num, page_size, 'last')
+
+    def get_link(self, page, size, rel):
+        replaced_uri = self.request.build_absolute_uri()
+        replaced_uri = replace_query_param(replaced_uri, self.page_query_param, page)
+        replaced_uri = replace_query_param(replaced_uri, self.page_size_query_param, size)
+        return '<{}>; rel="{}"'.format(replaced_uri, rel)
 
 # Using class-based views
 class TradePartnerList(views.APIView):
