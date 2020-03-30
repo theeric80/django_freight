46171420cd1eb4082ceab12660ec0eb26e2be90c
Eric Tsai <chunchieh@gmail.com>
2020-03-30 16:33:40 +0800

    support commodity and inventory pagination by REST framework pagination

--- django_freight/settings.py
+++ django_freight/settings.py
@@ -126,3 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# REST framework
+# https://www.django-rest-framework.org/api-guide/settings/
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 30
+}
