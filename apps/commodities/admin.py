2a159e2a227f294bc8242ac6dc92d500f539c7f1
Eric Tsai <chunchieh@gmail.com>
2020-04-02 08:16:07 +0800

    register django admin ModelAdmin classes

--- apps/commodities/admin.py
+++ apps/commodities/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from apps.commodities import models
+
 # Register your models here.
+@admin.register(models.TradePartner)
+class TradePartnerAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Commodity)
+class CommodityAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Inventory)
+class InventoryAdmin(admin.ModelAdmin):
+    pass
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from apps.users import models
 
 # Register your models here.
+class UserAdmin(BaseUserAdmin):
+    pass
+
+admin.site.register(models.User, UserAdmin)
